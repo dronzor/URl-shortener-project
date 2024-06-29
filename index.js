cdd2873a8792a9e135bde6a886a4570f6c954408
Avijit Dey <adey2860@gmail.com>
2024-06-29 21:00:52 +0530

    implementing server side rendering using ejs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express=require("express")
+const path=require("path")
 const urlRoute =require("./routes/url.js")
+const staticRouter=require('./routes/staticRouter.js')
 const {connectmongoDb}=require("./dbConnect.js")
 const URL=require("./models/url.js")
 
@@ -9,12 +11,18 @@ const PORT=8001
 connectmongoDb( "mongodb://127.0.0.1:27017/URL-shortener")
 .then(()=>console.log("database connected"))
 
+app.set('view engine','ejs')
+app.set("views",path.resolve("./views"))
 
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.use("/url",urlRoute)
 
-app.get("/:shortid",async(req,res)=>{
+app.use('/',staticRouter)
+
+
+app.get("/url/:shortid",async(req,res)=>{
     const shortId=req.params.shortid
     const entry=await URL.findOneAndUpdate(
         {
